b7d5e825fc0b5858259f35d87efca682c8bf2993
Gru <tnjbaalaguru@gmial.com>
2023-09-24 09:25:05 +0530

    two sum changed

---
+++ problems/Two_Sum.java
@@ -0,0 +1,19 @@
+import java.*;
+class Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        int sum;
+        Scanner in = new Scanner(System.in);
+        sum = in.nextInt();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int i=0; i<arr.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
